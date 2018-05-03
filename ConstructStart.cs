7c6f2928f98acb213f26b9e0f13ae9b6b338c5cb
ycryptx <yonatan.med@gmail.com>
2018-05-03 14:55:40 -0400

    add

---
+++ ConstructStart.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructStart : MonoBehaviour {
+
+	//have an array here
+	public GameObject[] array;
+	public Vector3 pos;
+//	private int x;		//apply collider on every second object
+	private bool checkDelete;	//checks we're not on first object to delete based on collider
+	public Quaternion rot;
+
+
+	// Use this for initialization
+	void Start () {
+		//array = new string[] {"A380","basketball","engineer","bb8", "buildings", "Canape", "church", "Cigarrete", "eb_house_plant_02","Fantasy yatch","ghetto","Jasubot-Pro01","microwave","Pizza","sheep","spino","computer","suv","TARDIS-FIGR_mkIII_station","Treadmill FBX","Tree","uh60","wood_house"};
+		array = Resources.LoadAll<GameObject>("");
+		pos = new Vector3(0, 0, 0);
+		rot = new Quaternion (0, 0, 0, 0);
+//		x = 0;
+//		spawnRandom ();
+//		spawnRandom ();
+
+		for (int i = array.Length - 1; i > 0; i--)
+		{
+			int swapIndex = Random.Range(0,i + 1);
+			spawnRandom (swapIndex);
+			GameObject tmp = array[i];
+			array[i] = array[swapIndex];
+			array[swapIndex] = tmp;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if viewer is within the Collider zone, call spawnRandom
+
+	} 
+
+	void spawnRandom (int rand) {
+		Random.InitState(System.DateTime.Now.Millisecond);
+//		int randomIndex = Random.Range(0,array.Length);
+//		Debug.Log (rand);
+//		Debug.Log (array[rand]);
+
+		pos.z = pos.z + Random.Range (100, 200);
+		pos.x = pos.x + Random.Range (-200, 200);
+		GameObject obj1 = (GameObject)Instantiate(array[rand], pos, rot);
+		SphereCollider sc = obj1.AddComponent<SphereCollider>() as SphereCollider;
+		sc.center = Vector3.zero;
+		sc.radius = 10;
+		sc.isTrigger = true;
+
+	
+
+//		if (x % 2 == 0) {
+//			x = x + 1;
+//			SphereCollider sc = obj1.AddComponent<SphereCollider>() as SphereCollider;
+//			sc.center = Vector3.zero;
+//			sc.radius = 3;
+//			sc.isTrigger = true;
+//		}
+
+
+
+
+	}
+
+//	void OnCollisionEnter(Collision col){
+//		if (Random.Range (0, 2) % 2 == 0) {
+////			Kino.AnalogGlitch glitch = new Kino.AnalogGlitch. (Camera.main);
+//			Kino.AnalogGlitch glitch = UnityStandardAssets.Characters.FirstPerson.FirstPersonController.
+//				gameObject.AddComponent<Kino.AnalogGlitch>() as Kino.AnalogGlitch;
+//
+//		} 
+//
+//	}
+
+
+
+
+}
+
---
+++ GridGenerator.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour {
+	[SerializeField] GameObject gridObject;
+
+	private bool gridObjectExists;
+	public int gridsToGenerate = 15;
+
+	private const float GRID_SPACE_HEIGHT = 9.61f;
+	//the grid transforms we want to change
+	private List<Transform> gridTransforms = new List<Transform>();
+	private List<MeshRenderer[]> gridMeshes = new List<MeshRenderer[]>();
+
+
+	public float zToLerp = -11.4f;
+
+	public float gridSpeed = 2f;
+	public float maxDistanceOfCamera = 2f;
+
+	public int currentGridHeader = 0;
+	public int lastGridElement = 0;
+
+	public Color startColor, endColor;
+
+	//happens as soon as the script starts
+	void Awake()
+	{
+		gridTransforms = new List<Transform> ();
+
+		gridObjectExists = gridObject != null;
+
+		if (gridObjectExists) {
+			GameObject previousGo = gridObject;
+			for (int i = 0; i < gridsToGenerate; i++) 
+			{
+				GameObject go = Instantiate (gridObject);
+				go.transform.position = previousGo.transform.position;
+				go.transform.position = new Vector3 (previousGo.transform.position.x, previousGo.transform.position.y, previousGo.transform.position.z - GRID_SPACE_HEIGHT);
+
+				//gets all the mesh renderers of the grid object
+				MeshRenderer[] rs = go.GetComponentsInChildren<MeshRenderer> ();
+				gridMeshes.Add (rs);
+				for (int j = 0; j < rs.Length; j++) 
+				{
+					rs [j].material.color = endColor;
+				}
+
+				gridTransforms.Add (go.transform);
+
+				previousGo = go;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (gridObjectExists) 
+		{
+			for (int i = 0; i < gridTransforms.Count; i++) 
+			{
+				Transform grid = gridTransforms [i];
+				grid.position = Vector3.MoveTowards (grid.position, new Vector3 (grid.position.x, grid.position.y, zToLerp - (GRID_SPACE_HEIGHT * i)), Time.deltaTime * gridSpeed);
+
+				float dist = Vector3.Distance (Camera.main.transform.position, gridTransforms [i].position);
+
+				// performance intensive, will call garbage collector often
+//				MeshRenderer[] rs = gridTransforms [i].GetComponentInChildren<MeshRenderer> ();
+				for (int j = 0; j < gridMeshes[i].Length; j++) 
+				{
+					//Lerp the grid color according to the distance between the object and the camera
+					gridMeshes[i][j].material.color = Color.Lerp(gridMeshes[i][j].material.color, endColor, Time.deltaTime * (dist / maxDistanceOfCamera) / 1000);
+				}
+
+				Vector3 toTarget = (Camera.main.transform.position - gridTransforms [i].position).normalized;
+				// if the grid object is behind the camera then set its position the same as GridObject
+				if (Vector3.Dot (toTarget, transform.forward) > 0)
+				{
+					Transform t = gridObject.transform;
+					gridTransforms [i].position = new Vector3 (t.position.x, t.position.y, t.position.z - GRID_SPACE_HEIGHT);
+					currentGridHeader++;
+
+					for (int j=0; j < gridMeshes[i].Length; j++)
+					{
+						gridMeshes[i][j].material.color = startColor;
+					}
+
+					if (currentGridHeader > gridTransforms.Count) 
+					{
+						currentGridHeader = 0;
+					}
+				}
+			}
+		}
+
+	}
+}
